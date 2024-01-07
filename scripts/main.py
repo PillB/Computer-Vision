8c1479dc78055036b03f2aca7a60af0a10f6419b
PillB <11358989+PillB@users.noreply.github.com>
2024-01-06 21:49:12 -0500

    Bounding boxes to list and only vehicles
    Implemented logic so save bounding boxes to list and filter for vehicles only

--- scripts/main.py
+++ scripts/main.py
@@ -3,11 +3,12 @@ import cv2
 
 # load models
 coco_model = YOLO('yolov8n.pt') # car detector model pretrain w COCO dataset
-license_plate_detector = YOLO('path to trained license plate model')
+#license_plate_detector = YOLO('path to trained license plate model')
 
 # load video
 cap = cv2.VideoCapture('./samples/sample1.mp4')
 
+vehicles = [2, 3, 5, 7,]
 # read frames
 frame_nmr = -1
 ret = True
@@ -17,4 +18,8 @@ while ret:
     if ret and frame_nmr < 10:
         # detect vehicles
         detections = coco_model(frame)[0] #[0] to do 1 step at a time
-        print(detections)
+        detections_ = [] #list where we will save all the bounding boxes our model detects
+        for detection in detections.boxes.data.toList():
+            x1, y1, x2, y2, score, class_id = detection #score is the confidence in the prediction, the class_id is the type of object
+            if int(class_id) in vehicles:
+                pass
